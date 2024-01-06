2b6fdbba9d94bc3543cf76c943d9e6383e9caf2c
HaD0207 <18050002360@163.com>
2024-01-06 19:35:25 +0800

    finish v1.0

---
+++ vue_cement/src/calculate/PH_Boilder.js
@@ -0,0 +1,175 @@
+// H锅炉的热量收支计算方程流程
+// 部分常量定义
+// 各个气体的体积分数
+const carbonMonoxideVolumeFraction = 0.1;
+const carbonDioxideVolumeFraction = 0.1;
+const waterVaporVolumeFraction = 0.1;
+const oxygenVolumeFraction = 0.2;
+const nitrogenVolumeFraction = 0.2;
+// 各个气体的常压下密度
+const carbonMonoxideDensity = 1.14;
+const carbonDioxideDensity = 1.98;
+const waterVaporDensity = 0.6;
+const oxygenDensity = 1.429;
+const nitrogenDensity = 0.81;
+const liquidWaterDensity = 1000;
+// 各个气体的比热
+const carbonMonoxideSpecificHeat = 1040;
+const carbonDioxideSpecificHeat = 840;
+const waterVaporSpecificHeat = 1850;
+const oxygenSpecificHeat = 918;
+const nitrogenSpecificHeat = 1040;
+const liquidWaterSpecificHeat = 4200;
+// 温度常量
+const waterVaporTemperature = 100;
+// 流程执行函数
+export function fun1(
+    hourlyWasteGasVolume,
+    hourlyClinkerProduction,
+    hourlyLiquidWaterVolume,
+    hourlyWaterVaporVolume,
+    ashConcentration,
+    ashSpecificHeat,
+    averageWasterGasSpecificHeat,
+    wasterGasTemperature,
+    wasterGasTemperaturePH,
+    liquidWaterTemperature
+){
+    let totalEnteringMassStream = cal_total_massStream_enteringNode(hourlyWasteGasVolume, hourlyClinkerProduction, hourlyLiquidWaterVolume, ashConcentration);
+    let totalEnteringSensible = cal_total_energyStream_enteringNode(hourlyWasteGasVolume, hourlyClinkerProduction, hourlyLiquidWaterVolume, wasterGasTemperature, ashConcentration, ashSpecificHeat, liquidWaterTemperature);
+    let totalLeavingMassStream = cal_total_massStream_leavingNode(hourlyWasteGasVolume, hourlyClinkerProduction, hourlyWaterVaporVolume, ashConcentration);
+    let totalLeavingSensible = cal_total_energyStream_leavingNode(hourlyWasteGasVolume, hourlyClinkerProduction, hourlyWaterVaporVolume, averageWasterGasSpecificHeat, wasterGasTemperaturePH, ashConcentration, ashSpecificHeat);
+
+    // 可计算物质流损失比和热效率
+    let massStreamRatio = totalLeavingMassStream / totalEnteringMassStream * 100;
+    let thermalEfficiency = totalLeavingSensible / totalEnteringSensible * 100;
+
+    return [massStreamRatio, thermalEfficiency];
+}
+
+// 1.进入节点的物质流总量
+function cal_total_massStream_enteringNode(hourlyWasteGasVolume, hourlyClinkerProduction, hourlyLiquidWaterVolume, ashConcentration){
+    // 进入锅炉的[预热器废气+预热器飞灰+液态水]质量总和
+    let wasterGasMassStream = cal_wasteGas_massStream_enteringNode(hourlyWasteGasVolume, hourlyClinkerProduction);
+    let ashMassStream = cal_Ash_massStream_enteringNode(ashConcentration, hourlyWasteGasVolume, hourlyClinkerProduction);
+    let liquidWaterMassStream = cal_liquidWater_massStream_enteringNode(hourlyLiquidWaterVolume, hourlyClinkerProduction);
+
+    let totalEnteringMassStream = wasterGasMassStream + ashMassStream + liquidWaterMassStream;
+    return totalEnteringMassStream;
+}
+// 1-1.预热器废气物质流公式
+function cal_wasteGas_massStream_enteringNode(hourlyWasteGasVolume, hourlyClinkerProduction){
+    let wasterGasStandardDensity;
+    let wasterGasMassStream;
+
+    wasterGasStandardDensity = (
+        (carbonDioxideVolumeFraction * carbonMonoxideDensity) +
+        (carbonMonoxideVolumeFraction * carbonDioxideDensity) +
+        (waterVaporVolumeFraction * waterVaporDensity) +
+        (oxygenVolumeFraction * oxygenDensity) +
+        (nitrogenVolumeFraction * nitrogenDensity)
+    ) / 100;
+
+    wasterGasMassStream = (hourlyWasteGasVolume / hourlyClinkerProduction) * wasterGasStandardDensity;
+
+    return wasterGasMassStream;
+}
+// 1-2.预热器飞灰物质流公式
+function cal_Ash_massStream_enteringNode(ashConcentration, hourlyWasteGasVolume, hourlyClinkerProduction){
+    let ashMassStream = (ashConcentration * hourlyWasteGasVolume) / hourlyClinkerProduction;
+    return ashMassStream;
+}
+// 1-3.液态水物质流公式
+function cal_liquidWater_massStream_enteringNode(hourlyLiquidWaterVolume, hourlyClinkerProduction){
+    let liquidWaterMassStream = (hourlyLiquidWaterVolume * liquidWaterDensity) / hourlyClinkerProduction;
+    return liquidWaterMassStream;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 2.进入节点的能量流总量
+function cal_total_energyStream_enteringNode(hourlyWasteGasVolume, hourlyClinkerProduction, hourlyLiquidWaterVolume, wasterGasTemperature, ashConcentration, ashSpecificHeat, liquidWaterTemperature){
+    let wasterGasSensible = cal_wasterGas_sensible_enteringNode(hourlyWasteGasVolume, hourlyClinkerProduction, wasterGasTemperature);
+    let ashSensible = cal_Ash_sensible_enteringNode(ashConcentration, hourlyWasteGasVolume, hourlyClinkerProduction, ashSpecificHeat, wasterGasTemperature);
+    let liquidWaterSensible = cal_liquidWater_sensible_enteringNode(hourlyLiquidWaterVolume, hourlyClinkerProduction, liquidWaterTemperature);
+
+    let totalEnteringSensible = wasterGasSensible + ashSensible + liquidWaterSensible;
+    return totalEnteringSensible;
+}
+// 2-1.废气的显热
+function cal_wasterGas_sensible_enteringNode(hourlyWasteGasVolume, hourlyClinkerProduction, wasterGasTemperature){
+    let wasterGasSpecificHeat;
+    let wasterGasSensible;
+
+    wasterGasSpecificHeat = (
+        (carbonMonoxideSpecificHeat * carbonMonoxideVolumeFraction) +
+        (carbonDioxideSpecificHeat * carbonDioxideVolumeFraction) +
+        (waterVaporSpecificHeat * waterVaporVolumeFraction) +
+        (oxygenSpecificHeat * oxygenVolumeFraction) +
+        (nitrogenSpecificHeat * nitrogenVolumeFraction)
+    ) / 100;
+    wasterGasSensible = (hourlyWasteGasVolume * wasterGasSpecificHeat * wasterGasTemperature) / hourlyClinkerProduction;
+    return wasterGasSensible;
+}
+// 2-2.飞灰的显热
+function cal_Ash_sensible_enteringNode(ashConcentration, hourlyWasteGasVolume, hourlyClinkerProduction, ashSpecificHeat, wasterGasTemperature){
+    let ashMassStream = cal_Ash_massStream_enteringNode(ashConcentration, hourlyWasteGasVolume, hourlyClinkerProduction);
+    let ashSensible = ashMassStream * ashSpecificHeat * wasterGasTemperature;
+    return ashSensible;
+}
+// 2-3.液态水的显热
+function cal_liquidWater_sensible_enteringNode(hourlyLiquidWaterVolume, hourlyClinkerProduction, liquidWaterTemperature){
+    let liquidWaterMassStream = cal_liquidWater_massStream_enteringNode(hourlyLiquidWaterVolume, hourlyClinkerProduction);
+    let liquidWaterSensible = liquidWaterMassStream * liquidWaterSpecificHeat * liquidWaterTemperature;
+    return liquidWaterSensible;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 3.离开节点的物质流总量
+function cal_total_massStream_leavingNode(hourlyWasteGasVolume, hourlyClinkerProduction, hourlyWaterVaporVolume, ashConcentration){
+    let wasterGasMassStream = cal_wasterGas_massStream_leavingNode(hourlyWasteGasVolume, hourlyClinkerProduction);
+    let ashMassStream = cal_Ash_massStream_leavingNode(ashConcentration, hourlyWasteGasVolume, hourlyClinkerProduction);
+    let waterVaporMassStream = cal_waterVapor_massStream_leavingNode(hourlyWaterVaporVolume, hourlyClinkerProduction);
+
+    let totalLeavingMassStream = wasterGasMassStream + ashMassStream + waterVaporMassStream;
+    return totalLeavingMassStream;
+}
+// 3-1.离开锅炉的废气物质流
+function cal_wasterGas_massStream_leavingNode(hourlyWasteGasVolume, hourlyClinkerProduction){
+    let wasterGasMassStream = cal_wasteGas_massStream_enteringNode(hourlyWasteGasVolume, hourlyClinkerProduction);
+    return wasterGasMassStream;
+}
+// 3-2.离开锅炉的飞灰物质流
+function cal_Ash_massStream_leavingNode(ashConcentration, hourlyWasteGasVolume, hourlyClinkerProduction){
+    let ashMassStream = cal_Ash_massStream_enteringNode(ashConcentration, hourlyWasteGasVolume, hourlyClinkerProduction);
+    return ashMassStream;
+}
+// 3-3.离开锅炉的水蒸气物质流
+function cal_waterVapor_massStream_leavingNode(hourlyWaterVaporVolume, hourlyClinkerProduction){
+    let waterVaporMassStream = (hourlyWaterVaporVolume * waterVaporDensity) / hourlyClinkerProduction;
+    return waterVaporMassStream;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 4.离开节点的能量总和
+function cal_total_energyStream_leavingNode(hourlyWasteGasVolume, hourlyClinkerProduction, hourlyWaterVaporVolume, averageWasterGasSpecificHeat, wasterGasTemperaturePH, ashConcentration, ashSpecificHeat){
+    let wasterGasSensible = cal_wasterGas_sensible_leavingNode(hourlyWasteGasVolume, hourlyClinkerProduction, averageWasterGasSpecificHeat, wasterGasTemperaturePH);
+    let ashSensible = cal_Ash_sensible_leavingNode(ashConcentration, hourlyWasteGasVolume, hourlyClinkerProduction, ashSpecificHeat, wasterGasTemperaturePH);
+    let waterVaporSensible = cal_waterVapor_sensible_leavingNode(hourlyWaterVaporVolume, hourlyClinkerProduction);
+
+    let totalLeavingSensible = wasterGasSensible + ashSensible + waterVaporSensible;
+    return totalLeavingSensible;
+}
+// 4-1.离开的废气显热
+function cal_wasterGas_sensible_leavingNode(hourlyWasteGasVolume, hourlyClinkerProduction, averageWasterGasSpecificHeat, wasterGasTemperaturePH){
+    let wasterGasSensible;
+    wasterGasSensible = hourlyWasteGasVolume * averageWasterGasSpecificHeat * wasterGasTemperaturePH / hourlyClinkerProduction;
+    return wasterGasSensible;
+}
+// 4-2.离开的飞灰显热
+function cal_Ash_sensible_leavingNode(ashConcentration, hourlyWasteGasVolume, hourlyClinkerProduction, ashSpecificHeat, wasterGasTemperaturePH){
+    let ashMassStream = cal_Ash_massStream_enteringNode(ashConcentration, hourlyWasteGasVolume, hourlyClinkerProduction);
+    let ashSensible = ashMassStream * ashSpecificHeat * wasterGasTemperaturePH;
+    return ashSensible;
+}
+// 4-3.离开的水蒸气显热
+function cal_waterVapor_sensible_leavingNode(hourlyWaterVaporVolume, hourlyClinkerProduction){
+    let waterVaporSensible = hourlyWaterVaporVolume * waterVaporSpecificHeat * waterVaporTemperature / hourlyClinkerProduction;
+    return waterVaporSensible;
+}
--- vue_cement/src/views/HomePage.vue
+++ vue_cement/src/views/HomePage.vue
@@ -4,7 +4,7 @@
       <!--   1-1: bread crumb   -->
       <el-page-header
           @back="isCollapse = !isCollapse"
-          title="Collapse"
+          :title="isCollapse? '展开菜单' : '关闭菜单'"
           :icon="null">
 <!--        <template #breadcrumb>-->
 <!--          <el-breadcrumb separator="/">-->
@@ -22,15 +22,15 @@
             />
             <span
                 class="text-large font-600 mr-3"
-                style="margin: 0 10px"
-            > Title   </span>
+                style="margin: 0 10px; font-size: 18px; font-weight: bolder; color: #808080"
+            > 热平衡模型计算系统   </span>
             <span
                 class="text-sm mr-2"
-                style="color: var(--el-text-color-regular);margin-right: 20px"
+                style="color: var(--el-text-color-regular);margin-right: 20px; font-size: 14px"
             >
-            Sub title
+            {{ title }}
           </span>
-            <el-tag>Default</el-tag>
+            <el-tag>白日模式</el-tag>
           </div>
         </template>
         <template #extra>
@@ -49,15 +49,13 @@
       <!--   2-1: menu options  -->
       <div class="menu_list">
         <!-- radio group and menu -->
-<!--        <el-button @click="isCollapse = !isCollapse">Click Me</el-button>-->
         <el-menu
           default-active="2"
           :collapse="isCollapse"
-          @open="handleOpen"
-          @close="handleClose"
           :collapse-transition="false"
+          class="menu_list"
         >
-          <el-menu-item v-for="(item, index) in menuList" :key="index" :index="item.index">
+          <el-menu-item v-for="(item, index) in menuList" :key="index" @click="changeModel(item.title)">
             <el-icon><component :is="item.icon" /></el-icon>
             <template #title>{{ item.title }}</template>
           </el-menu-item>
@@ -71,13 +69,16 @@
           <el-upload
             ref="uploadRef"
             :auto-upload="false"
+            :limit="1"
+            :file-list="uploadFiles"
+            :on-change="handleSuccess"
           >
             <template #trigger>
-              <el-button type="default" style="margin-right: 20px">uploadFile</el-button>
+              <el-button type="default" style="margin-right: 20px">上传文件</el-button>
 
             </template>
             <el-button type="primary" @click="handleUpload">
-              analysis
+              文件解析
             </el-button>
           </el-upload>
         </div>
@@ -87,35 +88,58 @@
           <div class="data_stream">
             <!--      2-2-2-1-1: input data      -->
             <el-scrollbar class="data_input">
-              <p>111</p>
+              <div class="output_title" >
+                <el-text style="font-size: 16px;font-weight: bolder;color: #808080">
+                  热量收支计算模型输入
+                  <el-icon><Odometer/></el-icon>
+                </el-text>
+              </div>
+              <el-form
+                  class="input_wrap"
+                  :model="inputList"
+                  :rules="formRules"
+                  ref="inputForm"
+              >
+                <el-form-item class="input_item" v-for="item in inputList" :key="item" :prop="[String(item.value)]">
+                  <div v-katex style="width: 60%;margin-right: 10px; font-size: 12px">{{ item.latexItem }} : {{ item.title }}</div>
+                  <el-input-number
+                    v-model="item.value"
+                    :precision="2"
+                    :step="1"
+                    class="input_component"
+                    size="small"
+                  />
+                </el-form-item>
+              </el-form>
+
             </el-scrollbar>
             <!--      2-2-2-1-2: output data      -->
             <el-scrollbar class="data_output">
               <!--       2-2-1-2-1: output title       -->
               <div class="output_title">
-                <el-text>
-                  Output data
+                <el-text style="font-size: 16px; font-weight: bolder; color: #808080;">
+                  模型输出及结果展示
                   <el-icon><Odometer/></el-icon>
                 </el-text>
-                <el-button>Button</el-button>
+                <el-button @click="starCalculate()">开始计算</el-button>
               </div>
               <!--       2-2-1-2-2: output draw       -->
               <div class="output_draw">
                 <div class="chart_item">
                   <div class="chart_title">
-                    chart_title
+                    流程对应物质流比值
                   </div>
                   <div class="pie_chart" ref="pieChart1"></div>
                 </div>
                 <div class="chart_item">
                   <div class="chart_title">
-                    chart_title
+                    模型对应热效率
                   </div>
                   <div class="pie_chart" ref="pieChart2"></div>
                 </div>
                 <div class="chart_item">
                   <div class="chart_title">
-                    chart_title
+                    模型对应热损失
                   </div>
                   <div class="pie_chart" ref="pieChart3"></div>
                 </div>
@@ -126,27 +150,31 @@
           </div>
           <!--     2-2-2-2: data introduce     -->
           <div class="data_introduce">
+            <el-table
+                :data="paramList"
+                stripe
+                style="width: 100%;height: 30%;margin-bottom: 10px;box-sizing: border-box;padding: 10px"
+            >
+              <el-table-column prop=latexItem label="符号" width="120" >
+                <template v-slot="scope">
+                  <div v-katex>{{scope.row.latexItem}}</div>
+                </template>
+              </el-table-column>
+              <el-table-column prop="value" label="数值" width="120" />
+              <el-table-column prop="title" label="参数介绍" width="270" />
+            </el-table>
             <div class="data_latex">
-              <el-text style="margin-bottom: 20px">相关公式</el-text>
+              <el-text style="margin-bottom: 20px;font-size: 16px;font-weight: bolder;color: #808080">相关公式</el-text>
               <el-carousel height="150px">
-                <el-carousel-item class="latex_wrap">
-                  <div v-katex class="latex_item">{{latexContent}}</div>
-                  <span class="latex_content">预热器飞灰计算公式</span>
-                </el-carousel-item>
-                <el-carousel-item>
-                  <div v-katex class="latex_item">
-                    {{latexContent}}
-                  </div>
-                </el-carousel-item>
-                <el-carousel-item>
-                  <div v-katex class="latex_item">
-                    {{latexContent}}
-                  </div>
+                <el-carousel-item class="latex_wrap" v-for="item in latexList" :key="item" >
+                  <div v-katex class="latex_item">{{ item.latexItem }}</div>
+                  <span class="latex_content">{{ item.latexContent }}</span>
                 </el-carousel-item>
               </el-carousel>
             </div>
+
             <el-scrollbar class="data_markdown">
-              <div v-html="renderedMarkDown"></div>
+              <div v-html="renderedMarkDown" style="font-size: 14px"></div>
             </el-scrollbar>
           </div>
         </div>
@@ -156,66 +184,97 @@
 </template>
 
 <script>
-import {Odometer} from "@element-plus/icons-vue";
+import {Odometer} from "@element-plus/icons-vue"
 import * as echarts from 'echarts'
 import katex from 'katex'
 import MarkdownIt from 'markdown-it'
+import cement_data from '@/data/cement_data.json'
+import {fun1} from '@/calculate/PH_Boilder';
+import * as XLSX from 'xlsx';
 export default {
   name: "HomePage",
   components: {Odometer},
   data(){
     return{
       isCollapse:true,
-      menuList:[
-        {index:1, icon:'Dish', title:'PH锅炉'},
-        {index:2, icon:'Cpu', title:'悬浮预热器'},
-        {index:3, icon:'Help', title:'分解炉'},
-        {index:4, icon:'Place', title:'回转窑'},
-        {index:5, icon:'IceCream', title:'冷却机'},
-        {index:6, icon:'PictureRounded', title:'AQC锅炉'},
-      ],
-      latexContent: '$$ \\rho_f=\\frac{C O_2^f \\times \\rho_{C O_2}+C O^f \\times \\rho_{C O}+O_2^f \\times \\rho_{O_2}+N_2^f \\times \\rho_{N_2}+H_2 O^f \\times \\rho_{H_2 O}}{100}\n $$',
-      markdownText: `### 6. AQC锅炉
-
-进入节点：来自冷却机的空气，飞灰，以及液态水
-
-| 冷却机空气 | mAk |
-| --- | --- |
-| 冷却机的飞灰 | mAfh |
-| 液态水 | mAw |
-
-进入节点的物质总和：mAk+mAfh+mAw
-
-进入节点的能量总和：QAk+QAfh+QAw
-
----
-
-离开节点：AQC锅炉排出冷却机空气和飞灰，以及水蒸气
-
-| AQC锅炉排出冷却机空气 | m’Ak | Q’Ak |
-| ------- | ------- | ------- |
-| 冷却机飞灰 | m’Afh | Q’Afh |
-| 水蒸气 | mAzq | QAzq |
-
-离开节点的物质总和：m’Ak+m’Afh+mAzq
-
-离开节点的能量总和：Q’Ak+Q’Afh+QAzq`
+      title:"",
+      // Collapse:"展开",
+      uploadFiles:[],
+      tempFile:"",
+      menuList:[],
+      // latexContent: '',
+      inputList:[],
+      latexList:[],
+      paramList:[],
+      markDownText:"",
+      // cement_data:cement_data.data[0]
+      formData:{},
+      formRules:{} // 动态生成表单验证规则
     }
   },
   methods:{
+    // 切换菜单
+    changeModel(title){
+      this.title = title
+    },
     // 处理上传文件
-    handleUpload(){
-      this.$refs.uploadRef.submit()
+    async handleUpload(){
+      let file = this.uploadFiles[0]
+      let reader = new FileReader()
+
+      const readAsArrayBuffer = (file) => {
+        return new Promise((resolve, reject) => {
+          reader.onload = (e) => resolve(e.target.result);
+          reader.onerror = reject;
+          reader.readAsArrayBuffer(file.raw);
+        });
+      };
+      try {
+        // 读取文件为 ArrayBuffer
+        const arrayBuffer = await readAsArrayBuffer(file);
+        // 将 ArrayBuffer 转换为 xlsx 工作簿
+        const workbook = XLSX.read(arrayBuffer, { type: 'array' });
+        // 假设您要读取第一个工作表
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+
+        // 将工作表数据转换为 JSON 对象
+        const jsonData = XLSX.utils.sheet_to_json(sheet);
+
+        const dataList = jsonData.map((row) => row);
+
+        this.inputList.forEach((item, index) => {
+          // 获取键名为 12 的值
+          item.value = dataList[index]['数值'];
+        });
+        // 读取txt
+        // this.tempFile = await readAsText(file);
+        // const lines = this.tempFile.split("\n");
+        // console.log(this.tempFile)
+        // const dataList = []
+        // lines.forEach((line) => {
+        //   // 将每行数据按逗号分割成两部分
+        //   const parts = line.split(',');
+        //   // 提取第二部分的数值并转换为浮点数
+        //   dataList.push(parseFloat(parts[1]));
+        // });
+        // this.inputList.forEach((item, index)=>{
+        //   item.value = dataList[index];
+        // })
+      } catch (error) {
+        console.error('读取文件失败:', error);
+      }
     },
     // 上传成功的处理
-    handleSuccess(){
-
+    handleSuccess(file, fileList){
+      this.uploadFiles = fileList
     },
-    createChart(param){
+    createChart(param, values){
       const myChart = echarts.init(param);
+      let percentage = (values * 100).toFixed(1);
       const option = {
         title: {
-          text: '40%',
+          text: percentage + "%" ,
           left: 'center',
           top: 61.5,
           textStyle:{
@@ -225,7 +284,6 @@ export default {
         visualMap: {
           show: false,
         },
-        // stillShowZeroSum: true,
         //鼠标划过时饼状图上显示的数据
         tooltip: {
           trigger: 'item',
@@ -239,7 +297,7 @@ export default {
           // itemWidth: 16,//图例颜色块的宽度和高度
           // itemHeight: 12,
           textStyle: {//图例中文字的样式
-            color: '#000',
+            color: '#808080',
             fontSize: 16
           },
           data: ['未领取', '处理中', '已完成']//图例上显示的饼图各模块上的名字
@@ -256,8 +314,8 @@ export default {
             center: ['50%', '50%'],
             data: [
               //itemSyle是单项的背景颜色设置。
-              { value: 60, itemStyle: { color: '#f1f1f1' } },
-              { value: 40, itemStyle: { color: '#1890fe' } },
+              { value: 100 - percentage, itemStyle: { color: '#f1f1f1' } },
+              { value: percentage, itemStyle: { color: '#1890fe' } },
             ],
             label: {
               //将视觉引导图关闭
@@ -265,7 +323,7 @@ export default {
             },
             itemStyle:{
               //设置的是每项之间的留白
-              borderWidth:7,
+              borderWidth:1,
               borderColor:'#fff'
             },
             // 初始化echarts的动画效果
@@ -278,12 +336,61 @@ export default {
         ]
       }
       myChart.setOption(option)
+    },
+    // 表单校验规则
+    validateNonZero(rule, value, callback) {
+      // 自定义校验规则：确保输入的数字不为0且为数字类型的数据
+      if (value !== 0.00 && value > 0 && !isNaN(value)) {
+        console.log("pass")
+        callback();
+      } else {
+        callback(new Error('请输入正确的数字，且不能为0'));
+        console.log("fail")
+      }
+    },
+    starCalculate(){
+      console.log(this.formRules)
+      this.$refs.inputForm.validate((valid)=>{
+        console.log("before validate")
+        // 通过验证
+        if(valid){
+          console.log("start validate")
+          let params = []
+          this.inputList.forEach((item)=>{
+            params.push(item.value)
+          })
+          let result = fun1(...params)
+          this.createChart(this.$refs.pieChart1, result[0])
+          this.createChart(this.$refs.pieChart2, result[1])
+          this.createChart(this.$refs.pieChart3, (1 - result[1]))
+        }else{
+          this.$message.error('请填写非0的数值信息');
+          this.$refs.inputForm.clearValidate(); // 清空表单项的错误信息
+          console.log("error validate")
+        }
+      })
+
     }
   },
   mounted() {
-    this.createChart(this.$refs.pieChart1)
-    this.createChart(this.$refs.pieChart2)
-    this.createChart(this.$refs.pieChart3)
+    this.createChart(this.$refs.pieChart1, 0)
+    this.createChart(this.$refs.pieChart2, 0)
+    this.createChart(this.$refs.pieChart3, 0)
+
+    this.inputList = JSON.parse(JSON.stringify(cement_data)).data[0].inputList
+    this.paramList = JSON.parse(JSON.stringify(cement_data)).data[0].paramList
+    this.menuList = JSON.parse(JSON.stringify(cement_data)).menuList
+    this.markDownText = JSON.parse(JSON.stringify(cement_data)).data[0].markDownText
+    this.latexList = JSON.parse(JSON.stringify(cement_data)).data[0].latexList
+
+    // 根据 formItems 动态生成表单验证规则
+    this.inputList.forEach((item)=>{
+      this.formRules[item.value] = [
+        { required: true, message: `${item.title}是必须输入`, trigger: 'blur' },
+        { validator: this.validateNonZero, message:`请输入非0且大于0的${item.title}`, trigger: 'blur' }
+      ];
+    })
+    console.log(this.formRules, "RULES");
   },
   computed:{
     renderedLatex(){
@@ -295,7 +402,9 @@ export default {
     },
     renderedMarkDown(){
       const md = new MarkdownIt()
-      return md.render(this.markdownText)
+      // return md.render(this.markdownText)
+      return md.render(this.markDownText)
+      // return md.render(JSON.stringify(cement_data.data[0].markDownText))
     }
   }
 }
@@ -321,10 +430,6 @@ html {
   height: 8vh;
   box-sizing: border-box;
   padding: 10px;
-  /*background-color: #cccccc;*/
-  /*display: flex;*/
-  /*align-items: center;*/
-  /*justify-content: space-between;*/
 }
 .home_body{
   width: 100%;
@@ -332,21 +437,20 @@ html {
   display: flex;
 }
 .menu_list{
-  /*flex-basis: 240px;*/
   height: 100%;
   box-sizing: border-box;
-  padding-right: 10px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   overflow: hidden;
+  flex-shrink: 0;
 }
 .analysis_content{
   box-sizing: border-box;
   padding: 0 10px;
   flex-grow: 1;
+  /*min-width: calc(95% - var(--el-menu-width));*/
   height: 100%;
-  /*background-color: plum;*/
   display: flex;
   flex-direction: column;
 }
@@ -383,11 +487,32 @@ html {
   background-color: #ffffff;
   box-sizing: border-box;
   margin-bottom: 10px;
+  padding: 10px;
+}
+.input_item{
+  width: 50%;
+  height: 40px;
+  /*background-color: #42b983;*/
+  display: flex;
+  box-sizing: border-box;
+  padding: 10px;
+  justify-content: space-between;
+  align-items: center;
+}
+.input_component{
+  width: 35%;
+}
+.input_wrap{
+  /*width: 100%;*/
+  display: flex;
+  flex-wrap: wrap;
 }
 .data_output{
   width: 100%;
   height: 50%;
   background-color: #ffffff;
+  box-sizing: border-box;
+  padding: 10px;
 }
 .output_title{
   width: 100%;
@@ -415,6 +540,9 @@ html {
   width: 100%;
   margin: 10px auto;
   text-align: center;
+  font-weight: bolder;
+  font-size: 14px;
+  color: #808080;
 }
 .pie_chart{
   width: 100%;
@@ -425,7 +553,7 @@ html {
 }
 .output_context{
   width: 100%;
-  height: 100px;
+  /*height: 100px;*/
   background-color: violet;
 }
 .data_introduce{
@@ -458,7 +586,7 @@ html {
   /*background-color: #42b983;*/
   background-color: white;
   width: 100%;
-  height: calc(75% - 10px);
+  height: calc(45% - 20px);
   overflow: hidden;
   box-sizing: border-box;
   padding: 10px;
@@ -559,12 +687,12 @@ html {
   width: 100%;
 }
 /deep/ .el-menu{
-  height: 70%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  /*height: 70%;*/
+  /*display: flex;*/
+  /*flex-direction: column;*/
+  /*justify-content: space-between;*/
 }
 /deep/ .el-menu-item{
-  /*margin-bottom: 60px;*/
+  width: max-content;
 }
 </style>
